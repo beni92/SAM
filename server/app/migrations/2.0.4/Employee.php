5d4c34e60b01fcb41b059b518bcb2697ff74430d
Benjamin Petterle <benjamin.petterle@toolsense.io>
2017-04-28 17:35:57 +0200

    Added Migration

---
+++ server/app/migrations/2.0.4/Employee.php
@@ -0,0 +1,88 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class EmployeeMigration_204
+ */
+class EmployeeMigration_204 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('Employee', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'userId',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'id'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('userId_UNIQUE', ['userId'], 'UNIQUE')
+                ],
+                'references' => [
+                    new Reference(
+                        'fk_Employee_1',
+                        [
+                            'referencedTable' => 'User',
+                            'columns' => ['userId'],
+                            'referencedColumns' => ['id'],
+                            'onUpdate' => 'NO ACTION',
+                            'onDelete' => 'NO ACTION'
+                        ]
+                    )
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '2',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'latin1_swedish_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
---
+++ server/app/migrations/2.0.4/Stock.php
@@ -0,0 +1,126 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class StockMigration_204
+ */
+class StockMigration_204 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('Stock', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'companyName',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'lastTradePrice',
+                        [
+                            'type' => Column::TYPE_DECIMAL,
+                            'size' => 20,
+                            'scale' => 4,
+                            'after' => 'companyName'
+                        ]
+                    ),
+                    new Column(
+                        'lastTradeTime',
+                        [
+                            'type' => Column::TYPE_DATETIME,
+                            'size' => 1,
+                            'after' => 'lastTradePrice'
+                        ]
+                    ),
+                    new Column(
+                        'stockExchange',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'lastTradeTime'
+                        ]
+                    ),
+                    new Column(
+                        'symbol',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 45,
+                            'after' => 'stockExchange'
+                        ]
+                    ),
+                    new Column(
+                        'floatShares',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'size' => 11,
+                            'after' => 'symbol'
+                        ]
+                    ),
+                    new Column(
+                        'marketCapitalization',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'size' => 11,
+                            'after' => 'floatShares'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '30676',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'latin1_swedish_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
