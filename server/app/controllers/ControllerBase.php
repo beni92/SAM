a6135e6ee335c219255c158f73836b4771ebd40d
amq <amq@users.noreply.github.com>
2017-05-01 13:45:19 +0200

    Add CORS headers

--- server/app/controllers/ControllerBase.php
+++ server/app/controllers/ControllerBase.php
@@ -7,5 +7,12 @@ class ControllerBase extends Controller
 {
     public function initialize() {
         $this->view->disable();
+
+        /**
+         * sets the CORS headers
+         */
+        $this->response->setHeader('Access-Control-Allow-Origin', '*');
+        $this->response->setHeader('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization');
+        $this->response->setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH, OPTIONS');
     }
 }
